0c69452af8edfe610781f53e77f9e0d82b5023f2
udara <udaradamitha@gmail.com>
2019-06-05 19:39:35 +0530

    rocket added and key events

---
+++ Assets/Rocket.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("pressed");
+
+        }
+         if (Input.GetKey(KeyCode.A))
+        {
+            print("left");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("right");
+        }
+    }
+}
