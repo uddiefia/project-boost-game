2d5590e01fce802e5d5ec10ab0c412ba31b5b176
udara <udaradamitha@gmail.com>
2019-06-06 16:23:32 +0530

    added multiple sounds and level 2 game state managemt

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
+
     Rigidbody rigidbody;
     AudioSource audioSource;
+    enum State{Alive,Dying,Transcending }
+    State state = State.Alive;
 
 	// Use this for initialization
 	void Start () {
@@ -16,10 +23,14 @@ public class Rocket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Thrust();
-        Rotate();
+        if (state== State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
+        
 	}
-    private void Rotate()
+    private void RespondToRotateInput()
     {
  
         float rotaionSpeed = rcsThrust * Time.deltaTime;
@@ -38,16 +49,12 @@ public class Rocket : MonoBehaviour {
 
     }
 
-    private void Thrust()
+    private void RespondToThrustInput()
     {
         
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidbody.AddRelativeForce(Vector3.up * (mainThrust*Time.deltaTime));
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
+            ApplyThrust();
         }
 
         else
@@ -55,21 +62,51 @@ public class Rocket : MonoBehaviour {
             audioSource.Stop();
         }
     }
+    private void ApplyThrust()
+    {
+        rigidbody.AddRelativeForce(Vector3.up * (mainThrust * Time.deltaTime));
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
 
      void OnCollisionEnter(Collision collision) {
+
+        if (state != State.Alive)
+        {
+            return;
+        }
 
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                print("friend");
+                
                 break;
-            case "Fuel":
-                print("fuel");
+            case "Finish":
+                state = State.Transcending;
+                audioSource.Stop();
+                audioSource.PlayOneShot(success);
+                Invoke("LoadNesxtScene",1f);
                 break;
             default:
-                print("dead");
+                state = State.Dying;
+                audioSource.Stop();
+                audioSource.PlayOneShot(death);
+                Invoke("LoadFirstLevel", 1f);
                 break;
         }
+
+    }
+
+   private  void LoadNesxtScene()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
 
     }
 }
